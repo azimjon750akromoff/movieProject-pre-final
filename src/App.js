cc202e9a88598254da459d11fae739a1b21b1fd5
azimjon750akromoff <azimjon750akromov@gmail.com>
2024-10-25 20:24:16 +0500

    my part

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Zeropage />} />
+          <Route path="/home" element={<Zeropage />} />
           <Route path="/search" element={<SearchResults />} />
         </Routes>
         <Routes>
